53fa71841186c248796276112828e67882950cf5
Shahrukh Naveed <62017710+SharkyND@users.noreply.github.com>
2023-02-11 16:36:55 -0500

    Time: 66 ms (24.83%), Space: 14.6 MB (44.43%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # split, sort, join, the function
+        
+        def splitSortJoin(str):
+            splitList = []
+            for element in str:
+                splitList.append(element)
+            
+            # sort them
+            splitList.sort()
+
+            # join 
+            return ''.join(splitList)
+
+        if splitSortJoin(s) == splitSortJoin(t):
+            return True
+        
+        return False
